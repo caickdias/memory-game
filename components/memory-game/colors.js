6213c1cbedf80725c8c598d1d510af56c52efe10
Caick Andrade <caickandrade0@gmail.com>
2021-05-30 20:20:27 -0300

    Add files via upload

---
+++ components/memory-game/colors.js
@@ -0,0 +1,46 @@
+export const themes = {
+    light: {
+        main: {
+            background: '#bbb',
+            newGameButton: 'black',
+            newGameButtonText: 'white',
+            selectorText: 'black',
+        },
+        card: {
+            faceDown: 'grey',
+            faceUp: 'white',  
+            faceUpText: 'black',     
+            border: 'black', 
+        },
+        endgame: {
+            background: '#eee',
+            newGameButton: 'black',
+            newGameButtonText: 'white',
+            selectorText: 'black',
+        },    
+    },
+    dark: {
+        main: {
+            background: '#212121',
+            newGameButton: 'grey',
+            newGameButtonText: 'black',
+            selectorTriangle: '#bdbdbd',
+            selectorText: 'white',
+        },
+        card: {
+            faceDown: '#424242',
+            faceUp: '#cfcfcf',  
+            faceUpText: 'black',    
+            border: 'grey',  
+        },
+        endgame: {
+            background: '#bdbdbd',
+            newGameButton: 'black',
+            newGameButtonText: 'white',
+            selectorTriangle: 'black',
+            selectorText: 'black',
+        },    
+    }
+}
+export default themes.light;
+
